9d275646e2a88b7f1df304836b27bf5885aaf685
Do Duong <duongdo990@gmail.com>
2019-06-21 19:28:55 +0700

    21/6/2019 7:28PM

--- Assignment/src/java/dao/UserDAO.java
+++ Assignment/src/java/dao/UserDAO.java
@@ -4,6 +4,7 @@ import dbconfig.DBConfig;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.Statement;
 import utils.MD5;
 
 import model.User;
@@ -11,12 +12,44 @@ import model.User;
 public class UserDAO {
 
     private static final String getUsrSt = "select * from USERS where UserName = ? and PassWord = ?";
-    private static final String insUserSt = "insert into USERS(UserName,PassWord,Email) values(?,?,?)";
+    private static final String insUserSt = "insert into USERS(UserName,PassWord,Email,Status) values(?,?,?,'False')";
     private static final String getAvtSt = "select * from USERS where UserName = ?";
     private static final String findUsername = "select UserName from USERS where Username = ?";
     private static final String findEmail = "select Email from USERS where Email=?";
+    private static final String update = "update USERS set FullName = ? ,Age = ? ,"
+            + "Gender = ? ,Status = True ,UserRight = 3 where UserName = ?";
+    private static final String checkInfo = "select UserName from USERS where Status = 'True'";
 
- 
+    public static boolean checkInfo(String username) {
+        try (Connection conn = DBConfig.getConnection()) {
+            Statement st = conn.createStatement();
+            ResultSet rs = st.executeQuery(checkInfo);
+            if (rs.next()) {
+                return true;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public static boolean updateUserInfo(User usr) {
+        try (Connection conn = DBConfig.getConnection()) {
+            PreparedStatement ps = conn.prepareStatement(update);
+            ps.setString(1, usr.getFullName());
+            ps.setString(2, Integer.toString(usr.getAge()));
+            ps.setString(3, usr.getGender());
+            ps.setString(4, usr.getUserName());
+            int result = ps.executeUpdate();
+            if (result > 0) {
+                return true;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
     public static boolean checkUsersName(String UserName) {
 
         try (Connection conn = DBConfig.getConnection()) {
@@ -46,7 +79,8 @@ public class UserDAO {
         }
         return false;
     }
-   // Đăng kí User
+    // Đăng kí User
+
     public static User register(String username, String password, String email) {
         User u = null;
         try (Connection con = DBConfig.getConnection()) {
@@ -110,6 +144,6 @@ public class UserDAO {
         return null;
     }
     public static void main(String[] args) {
-        System.out.println(checkEmail("a@b.c"));
+        System.out.println(checkInfo("abc"));
     }
 }
